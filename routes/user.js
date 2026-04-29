7875f369cea9d848735af1b7e5ce40c7035482f9
Malvaxz <malvaezjr.mtz@outlook.com>
2026-04-29 13:11:33 -0600

    uso de jwt

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');//conexion a mi bd
 
@@ -17,13 +18,32 @@ user.post("/", async (req, res, next) => {
     return res.status(500).json({ code: 500, message: "Error inserting data", error });
 });
 
+user.post("/login", async (req, res, next) => {
+    const { user_email, user_password } = req.body;
+    const query = `SELECT * FROM user WHERE user_email = '${user_email}' AND user_password = '${user_password}'`;
+    const rows = await db.query(query);
+    console.log(rows);
+    if (user_email && user_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({ 
+                user_id: rows[0].user_id,
+                user_email: rows[0].user_email
+            }, "debugkey");
+            return res.status(200).json({ code: 200, message: token });
+        }
+        else {
+            return res.status(401).json({ code: 401, message: "Invalid email or password" });
+        }
+    }
+});
+
 user.get("/", async (req, res, next) => {
     try {
         const query = "SELECT * FROM user";
         const rows = await db.query(query);
         if (rows.length > 0) {
             return res.status(200).json({ code: 200, message: "Users retrieved successfully", data: rows });
-        }       
+        }
         return res.status(404).json({ code: 404, message: "No users found" });
     }
     catch (error) {
